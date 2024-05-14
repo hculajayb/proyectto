0a9383dc35c1a991df1c7121da909d74899f3918
hculajay <hculajayb@miumg.edu.gt>
2024-05-13 19:37:56 -0600

    Para publicar en Serpa HC

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -1,6 +1,13 @@
 import postgres from "postgres";
 
 //conexion a la base de datos foodservice
-const dbConnect = postgres('postgres://foodservice_user:5T3TsgzYzthVflvKtwuOJKHzZhVuWwlv@dpg-covms0g21fec73fniarg-a:5432/foodservice');
+
+//Render
+//const dbConnect = postgres('postgres://foodservice_user:5T3TsgzYzthVflvKtwuOJKHzZhVuWwlv@dpg-covms0g21fec73fniarg-a:5432/foodservice');
+
+//Serpa
+const dbConnect = postgres('postgres://foodservice_user:5T3TsgzYzthVflvKtwuOJKHzZhVuWwlv@dpg-covms0g21fec73fniarg-a.oregon-postgres.render.com:5432/foodservice');
+
+//Localhost
 //const dbConnect = postgres('postgres://postgres:root@localhost:5432/foodservice');
 export default dbConnect;
