c055dd3fd5afda470d21dc484f5bba0c1a8b7448
hculajay <hculajayb@miumg.edu.gt>
2024-05-14 21:42:45 -0600

    Para publicar en Serpa intento 2 HC

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -3,10 +3,15 @@ import postgres from "postgres";
 //conexion a la base de datos foodservice
 
 //Render
-//const dbConnect = postgres('postgres://foodservice_user:5T3TsgzYzthVflvKtwuOJKHzZhVuWwlv@dpg-covms0g21fec73fniarg-a:5432/foodservice');
+const dbConnect = postgres('postgres://foodservice_user:5T3TsgzYzthVflvKtwuOJKHzZhVuWwlv@dpg-covms0g21fec73fniarg-a.oregon-postgres.render.com:5432/foodservice', {
+    ssl: {
+        // Habilitar SSL
+        rejectUnauthorized: false // Configura para aceptar certificados autofirmados, en producción, usa certificados de confianza
+    }
+});
 
 //Serpa
-const dbConnect = postgres('postgres://foodservice_user:5T3TsgzYzthVflvKtwuOJKHzZhVuWwlv@dpg-covms0g21fec73fniarg-a.oregon-postgres.render.com:5432/foodservice');
+//const dbConnect = postgres('postgres://foodservice_user:5T3TsgzYzthVflvKtwuOJKHzZhVuWwlv@dpg-covms0g21fec73fniarg-a.oregon-postgres.render.com:5432/foodservice');
 
 //Localhost
 //const dbConnect = postgres('postgres://postgres:root@localhost:5432/foodservice');
